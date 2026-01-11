5ad3ec2d261316f596a314a2cd9553a3919e3ccb
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-11 12:19:17 +0530

    Add Some Effects In About Section

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,8 +1,12 @@
+'use client'
 import React from 'react'
-
+import { File, Search, Settings } from "lucide-react"
+import { OrbitingCirclesDemo } from '../ui/OrbitingCircles'
 const About = () => {
   return (
-    <div className='h-screen'>About</div>
+    <>
+      <OrbitingCirclesDemo />
+    </>
   )
 }
 
