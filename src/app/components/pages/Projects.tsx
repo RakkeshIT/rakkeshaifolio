9227f870f2c75ff816e7500b51c0a330f302c817
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-14 14:38:50 +0530

    fix: adjust Projects component to use min-h-screen for better layout

--- src/app/components/pages/Projects.tsx
+++ src/app/components/pages/Projects.tsx
@@ -4,7 +4,7 @@ import Laravel1 from "../assets/Projects/Laravel1.png"
 import ProjectCard from '../layouts/Card'
 const Projects = () => {
   return (
- <div className="h-screen w-full px-4 py-10">
+ <div className="min-h-screen w-full px-4 py-10">
   {/* Card Container */}
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
     <ProjectCard
