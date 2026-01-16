7e6f8e93be298a25f6b15ed17b5c299207964fa7
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-16 11:18:45 +0530

    feat: add @types/axios dependency and clean up coreAPI import

--- src/services/coreAPI.ts
+++ src/services/coreAPI.ts
@@ -1,4 +1,3 @@
-// lib/coreAPI.ts
 import axios from "axios";
 
 const baseURL = process.env.NODE_ENV == 'development' ? process.env.NEXT_PUBLIC_BASE_URL_DEVELOPMENT : process.env.NEXT_PUBLIC_BASE_URL;
