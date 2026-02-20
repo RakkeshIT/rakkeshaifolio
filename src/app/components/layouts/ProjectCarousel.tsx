a75248cb27a980b86ed1e1657ccf34520e190358
RakkeshIT <rakkeshit@gmail.com>
2026-02-20 17:44:07 +0530

    .

--- src/app/components/layouts/ProjectCarousel.tsx
+++ src/app/components/layouts/ProjectCarousel.tsx
@@ -9,29 +9,28 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import axios from "axios";
-import { span } from "framer-motion/client";
 interface props {
   folder: string;
 }
 const ProjectCarousel = ({ folder }: props) => {
-  const [images, setImages] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false)
-  console.log("folder: ", folder)
+  const [images, setImages] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
+  console.log("folder: ", folder);
   useEffect(() => {
     const fetchImage = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
-        const res = await axios.get(`/api/project-images?folder=${folder}`)
-      const data: any = res.data;
-      setImages(data)
+        const res = await axios.get<string[]>(`/api/project-images?folder=${folder}`);
+        const data = res.data;
+        setImages(data);
       } catch (error) {
         console.log("Errom form fethc images", error);
-      }finally{
-        setLoading(false)
+      } finally {
+        setLoading(false);
       }
-    }
-    fetchImage()
-  }, []);
+    };
+    fetchImage();
+  }, [folder]);
   return (
     <Carousel className="h-full w-full">
       <CarouselPrevious />
@@ -39,15 +38,15 @@ const ProjectCarousel = ({ folder }: props) => {
       <CarouselContent>
         {images.map((image, i) => (
           <CarouselItem key={i} className="h-full w-full">
-            <img src={image} alt=""/>
+            <img src={image} alt="" />
           </CarouselItem>
         ))}
 
-      {loading && (
-  <div className="flex flex-col items-center justify-center py-20">
-    <div className="w-10 h-10 border-4 border-indigo-300 border-t-indigo-600 rounded-full animate-spin mb-4"></div>
-    <span className="text-sm text-gray-600 dark:text-gray-300 animate-pulse">
-      Loading Skills...
+       {loading && (
+  <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/40 backdrop-blur-sm z-50">
+    <div className="w-10 h-10 border-4 border-indigo-300 border-t-indigo-600 rounded-full animate-spin mb-4" />
+    <span className="text-sm text-white animate-pulse">
+      Loading Images...
     </span>
   </div>
 )}
